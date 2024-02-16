3a82fc19ea3dc64f2f4270ab9d43beab4895acbb
Ts.No.42 <147478079+LuXuanEr@users.noreply.github.com>
2024-02-16 17:43:55 +0800

    First strart
    make player move horizontal and verticle

---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed,jumpHigh;
+    private float xInput;
+
+    private float yInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+       
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y );
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(xInput * moveSpeed, jumpHigh );
+        }
+
+
+    }
+}
