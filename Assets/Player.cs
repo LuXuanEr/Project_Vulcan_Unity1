ed1df5bc0a80e134e0677e4bc38fd6e24893c7be
Ts.No.42 <147478079+LuXuanEr@users.noreply.github.com>
2024-02-16 20:55:00 +0800

    2024年2月16中文日志-角色动画和移动
    完成了角色的奔跑动画以及实现二段跳，从现在开始日志将以中文记录

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,34 +1,103 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float moveSpeed,jumpHigh;
+    private Rigidbody2D rb;
+    private Animator anim;
+    [SerializeField] private float moveSpeed,jumpHigh;
     private float xInput;
+    
+    private int FacingDir = 1;
+    private bool facingRight = true;
+    private bool isGrounded;
+    private int Jumpcounter = 0;
+    
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
 
-    private float yInput;
-    // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+
+    void Update()
+    {
+        Movement();
+        
+        CheckInput();
+
+        isGrounded = Physics2D.Raycast(transform.position,Vector2.down,groundCheckDistance,whatIsGround);
+        
+        Debug.Log(isGrounded);
+        
+        FlipController();
+
+        AnimatorController();
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) )
+        {
+            Jump();
+            Jumpcounter -= 1;
+        }else if (isGrounded)
+        {
+            Jumpcounter = 1;
+        }
+
+       
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y );
+    }
+
+    private void Jump()
+    {   
+        if(isGrounded || Jumpcounter > 0)
+        rb.velocity = new Vector2(xInput * moveSpeed, jumpHigh );
+    }
+
+    private void AnimatorController()
+    {
+        
+        bool IsMoving = rb.velocity.x != 0;
+        
+        anim.SetBool("IsMoving", IsMoving);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Flip()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
+        FacingDir = FacingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0,180,0);
+    }
 
-       
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y );
-        
-
-        if (Input.GetKeyDown(KeyCode.Space))
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight)
         {
-            rb.velocity = new Vector2(xInput * moveSpeed, jumpHigh );
+            Flip();
         }
+        else if(rb.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
 
-
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
